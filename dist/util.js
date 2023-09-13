c800465ca8a5e28f64cb9c3070b312736fd8182f
Ravikumar <ravikumar.rajagopalan@cleartax.in>
2023-09-13 09:44:55 +0530

    Delete dist directory

--- dist/util.js
+++
@@ -1,1 +0,0 @@
-importScripts("https://unpkg.com/comlink/dist/umd/comlink.js");const heavyOperation=()=>{let e=[];for(let o=2;o<=1e7;o++){let t=!0;for(let e=2;e*e<=o;e++)if(o%e==0){t=!1;break}t&&e.push(o)}return console.log("Prime calculation finished"),e};Comlink.expose({heavyOperation});
