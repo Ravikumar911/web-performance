1b0269e87c7a2a108369206fd40acf731ebea8a6
Ravikumar911 <ravikumar.rajagopalan@cleartax.in>
2023-09-13 10:28:40 +0530

    fix url

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 //@ts-expect-error
 import Nav from 'header-remote/Nav';
+import { OverviewCard } from '@cleartax/mtsg-ui';
 // import { MainThread } from './components/MainThread';
 // import { WorkerThread } from './components/WorkerThread';
 
@@ -13,6 +14,7 @@ const App: React.FC = () => {
   return (
     <>
       <Nav></Nav>
+      <OverviewCard />
       <div className="mt-10 text-3xl mx-auto max-w-6xl">
         <Tailwind />
       </div>
