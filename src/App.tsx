ab08abbdb01fc218800e5fdcb80bffcc42d11831
Ravikumar911 <ravikumar.rajagopalan@cleartax.in>
2023-09-13 16:26:14 +0530

    chagn

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// @ts-expect-error
-// import Nav from 'header-remote/Nav';
-// //@ts-expect-error
-// import FormsHanson from 'header-remote/FormsHanson';
-// //@ts-expect-error
-// import Forms3D from 'header-remote/Forms3D';
+@ts-expect-error
+import Nav from 'header-remote/Nav';
+//@ts-expect-error
+import FormsHanson from 'header-remote/FormsHanson';
+//@ts-expect-error
+import Forms3D from 'header-remote/Forms3D';
 
-// import ThreeJSComponent from './components/Launchpad';
-// import Handsontable from './components/Handsontable';
-// import Card from './components/Card';
+import ThreeJSComponent from './components/Launchpad';
+import Handsontable from './components/Handsontable';
+import Card from './components/Card';
 
 // // @ts-ignore
 // const Nav = React.lazy(() => import('header-remote/Nav'));
@@ -22,13 +22,28 @@ import { createRoot } from 'react-dom/client';
 // const Handsontable = React.lazy(() => import('./components/Handsontable'));
 // const Card = React.lazy(() => import('./components/Card'));
 
-import { MainThread } from './components/MainThread';
 import './index.scss';
 
 const App: React.FC = () => {
   return (
     <>
-      <MainThread />
+      <h1>Loaded from App2 Microfrontend</h1>
+      <Nav></Nav>
+      <Forms3D />
+      <div className="mt-10 text-3xl mx-auto max-w-6xl">
+        <FormsHanson />
+      </div>
+      <div className="bg-gray-50 mb-16">
+        <h1 className="mt-10">Loaded from App1 Microfrontend</h1>
+        <ThreeJSComponent />
+        <div className="mt-10 text-3xl mx-auto max-w-6xl">
+          <Card />
+          <div className="my-6">
+            <h1>Summary Table</h1>
+          </div>
+          <Handsontable />
+        </div>
+      </div>
     </>
   );
 };
