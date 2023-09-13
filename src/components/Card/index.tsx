3f6ef9b930dfc608ce270a8bae19e9d8ce358009
Ravikumar911 <ravikumar.rajagopalan@cleartax.in>
2023-09-13 12:28:51 +0530

    update

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,52 +1,35 @@
-import React, { useEffect } from 'react';
-import * as THREE from 'three';
+import React from 'react';
+import moment from 'moment';
 
-function ThreeJSComponent() {
-  useEffect(() => {
-    let mount = document.getElementById('threejs-canvas');
-
-    // Create a scene
-    const scene = new THREE.Scene();
-
-    // Create a camera
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    camera.position.z = 5;
-
-    // Create a renderer
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    mount.appendChild(renderer.domElement);
-
-    // Create a cube
-    const geometry = new THREE.BoxGeometry();
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-
-    // Add the cube to the scene
-    scene.add(cube);
-
-    // Create an animation loop
-    const animate = function () {
-      requestAnimationFrame(animate);
-
-      // Rotate the cube
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-      // Render the scene from the perspective of the camera
-      renderer.render(scene, camera);
-    };
-
-    // Start the animation loop
-    animate();
-  }, []);
-
-  return <div id="threejs-canvas"></div>;
-}
-
-export default ThreeJSComponent;
+export const Card = () => {
+  return (
+    <>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg">
+        <div className="px-6 py-4">
+          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+          <p className="text-gray-700 text-base">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Voluptatibus quia, nulla! Maiores et perferendis eaque,
+            exercitationem praesentium nihil. posted on &nbsp;
+            <span className="font-semibold bg-purple-900 text-white rounded px-2 py-1 mt-2">
+              {moment(moment.now())
+                .subtract(Math.floor(Math.random() * 10), 'd')
+                .format('DD-MMM-YYYY hh:ss')}
+            </span>
+          </p>
+        </div>
+        <div className="px-6 pt-4 pb-2">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #photography
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #travel
+          </span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #winter
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/components/Launchpad/index.tsx
+++ src/components/Launchpad/index.tsx
@@ -1,21 +1,52 @@
-import React from 'react';
-import { LaunchpadCards } from '@cleartax/mtsg-ui';
+import React, { useEffect } from 'react';
+import * as THREE from 'three';
 
-export const Launchpad = () => {
-  return (
-    <LaunchpadCards
-      {...{
-        icon: 'https://assets1.cleartax-cdn.com/cleartax/images/1644238215_lp2icons1.svg',
-        backgroundColorClass: 'bg-orange-400',
-        cardName: 'GSTR 1',
-        cardDescription:
-          'Monthly/Quarterly return for declaring outward supplies',
-        onCardClick: () => console.log('hi'),
-        newTag: true,
-        newTagBackgroundColorClass: 'bg-orange-500',
-        isAccessible: true,
-        includeLicensingWrapper: true,
-      }}
-    />
-  );
-};
+function ThreeJSComponent() {
+  useEffect(() => {
+    let mount = document.getElementById('threejs-canvas');
+
+    // Create a scene
+    const scene = new THREE.Scene();
+
+    // Create a camera
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
+    camera.position.z = 5;
+
+    // Create a renderer
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    mount.appendChild(renderer.domElement);
+
+    // Create a cube
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+
+    // Add the cube to the scene
+    scene.add(cube);
+
+    // Create an animation loop
+    const animate = function () {
+      requestAnimationFrame(animate);
+
+      // Rotate the cube
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+
+      // Render the scene from the perspective of the camera
+      renderer.render(scene, camera);
+    };
+
+    // Start the animation loop
+    animate();
+  }, []);
+
+  return <div id="threejs-canvas"></div>;
+}
+
+export default ThreeJSComponent;
