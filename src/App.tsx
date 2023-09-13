d0b4d41f91b5fdac9e03706107692fa5d92322ff
Ravikumar911 <ravikumar.rajagopalan@cleartax.in>
2023-09-13 15:38:13 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,21 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-// //@ts-expect-error
-// import Nav from 'header-remote/Nav';
-// //@ts-expect-error
-// import FormsHanson from 'header-remote/FormsHanson';
-// //@ts-expect-error
-// import Forms3D from 'header-remote/Forms3D';
+//@ts-expect-error
+import Nav from 'header-remote/Nav';
+//@ts-expect-error
+import FormsHanson from 'header-remote/FormsHanson';
+//@ts-expect-error
+import Forms3D from 'header-remote/Forms3D';
 
-// import ThreeJSComponent from './components/Launchpad';
-// import Handsontable from './components/Handsontable';
-// import Card from './components/Card';
-
-// @ts-ignore
-const Nav = React.lazy(() => import('header-remote/Nav'));
-// @ts-ignore
-const FormsHanson = React.lazy(() => import('header-remote/FormsHanson'));
-// @ts-ignore
-const Forms3D = React.lazy(() => import('header-remote/Forms3D'));
-
-const ThreeJSComponent = React.lazy(() => import('./components/Launchpad'));
-const Handsontable = React.lazy(() => import('./components/Handsontable'));
-const Card = React.lazy(() => import('./components/Card'));
+import ThreeJSComponent from './components/Launchpad';
+import Handsontable from './components/Handsontable';
+import Card from './components/Card';
 
 import './index.scss';
 
 const App: React.FC = () => {
   return (
-    <React.Suspense fallback={<div className="spinner"></div>}>
+    <>
       <h1>Loaded from App2 Microfrontend</h1>
       <Nav></Nav>
       <Forms3D />
@@ -44,7 +33,7 @@ const App: React.FC = () => {
           <Handsontable />
         </div>
       </div>
-    </React.Suspense>
+    </>
   );
 };
 
